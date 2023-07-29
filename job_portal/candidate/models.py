906598b4a2b1479d204e079409595c12fc64b448
anima <sahilkalathiya0120@gmail.com>
2023-07-29 19:59:31 -0400

    Tracking Complete.

--- job_portal/candidate/models.py
+++ job_portal/candidate/models.py
@@ -26,7 +26,3 @@ class Application(models.Model):
         Candidate, on_delete=models.SET_NULL, null=True, blank=True)
     job = models.ForeignKey(Job, on_delete=models.CASCADE)
     status = models.CharField(max_length=255)
-
-
-class Tracking(models.Model):
-    date = models.DateField()
